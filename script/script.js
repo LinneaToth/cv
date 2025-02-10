6ccf321e56173bf0f373a1ba44223589ed0ceb22
Linnéa Toth <linnea@liroit.com>
2025-02-10 11:05:37 +0100

    work done on responsive layout. separate style sheets added with specific css for each page.

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ console.log("this works?");
 
 const menuButton = document.querySelector(".menu-toggle-btn");
 const navigationMenu = document.getElementById("menu");
+const gridBody = document.querySelector("body");
 
 
 console.log("this works?");
@@ -12,6 +13,7 @@ const menuVisibility = function () {
     navigationMenu.classList.toggle("hidden");
     menuButton.classList.toggle("fa-bars");
     menuButton.classList.toggle("fa-x");
+    gridBody.classList.toggle("static");
     console.log("this works");
 }
 
