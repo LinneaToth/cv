05de0767e7051e81c62bf168723f47681386340f
Linnéa Toth <linnea@liroit.com>
2025-02-13 16:02:15 +0100

    comments added to script.js

--- script/script.js
+++ script/script.js
@@ -1,22 +1,18 @@
-console.log("this works?");
-
-
+/* menu button, the menu itself and the body element are selected and assigned to variables */
 const menuButton = document.querySelector(".menu-toggle-btn");
 const navigationMenu = document.getElementById("menu");
 const gridBody = document.querySelector("body");
 
 
-console.log("this works?");
-
-
+/* a simple function that does the following: Toggling visibility, changing if the menu button icon is showing bars or a cross, locks the screen in place beneath the menu when it is activated */
 const menuVisibility = function () {
     navigationMenu.classList.toggle("hidden");
     menuButton.classList.toggle("fa-bars");
     menuButton.classList.toggle("fa-x");
     gridBody.classList.toggle("static");
-    console.log("this works");
 }
 
+/* listening for a click on the menu button, running the function above when it gets one */
 menuButton.addEventListener("click", menuVisibility);
 
 
